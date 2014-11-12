f1c0322b27ddc432941f5f3ec4c4fb122860e429
rascarlo <rascarlo@gmail.com>
2014-11-12 14:34:17 +0100

    volume rocker: volume key cursor control

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2646,6 +2646,15 @@ public final class Settings {
         public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
 
         /**
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
