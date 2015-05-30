1ac2ba38815fcb25d604c86258c7f69576b022ca
Yashar Ahmedizade <yashade2001@gmail.com>
2015-05-31 00:18:16 +0200

    Add option to disable searchbar at recents [2/2]
    coded by yashade2001 @ projectdisco

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2640,6 +2640,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
 
         /**
+         * Recents Show/Hide Search Bar
+         * @hide
+         */
+        public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
