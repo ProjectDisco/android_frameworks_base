2a8f61b66020e196fc3df86d0bceded30dbb46ad
Yashar Ahmedizade <yashade2001@gmail.com>
2015-10-24 20:32:48 +0300

    Navigation bar custom height (1/2)
    ------------------------------
    -- Original author: Quarx2k --
    ------------------------------
    
    I've just forward-ported his commit ( https://github.com/rascarlo/android_frameworks_base/commit/85ce62a941486d27e30d3727a63817de783ef241 ).

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3239,6 +3239,12 @@ public final class Settings {
          */
 
         /**
+         * Height of navigation bar buttons
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
